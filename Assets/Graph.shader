a6d4ef218903e4ea37b9e00ccc83df6be08c0043
machinetown <machinetown44@gmail.com>
2020-07-30 14:20:49 +0700

    Initial upload

---
+++ Assets/Graph.shader
@@ -0,0 +1,104 @@
+﻿Shader "Custom/Graph"
+{
+    Properties
+    {
+        _MainTex ("Lookup Texture", 2D) = "white" {}
+        _SegmentData("Segment data", 2D) = "black" {}
+        _Thicc("Thickness", Range(0,0.02)) = 0.002
+        _BackColor("Background color", Color) = (1, 0, 0, 1)
+        _BackFade("Background fade", Range(0, 1)) = 0
+        _BackOffset("Background offset", Range(0, 2)) = 0
+        _FrontColor("Foreground color", Color) = (0, 1, 0, 1)
+        _FrontFade("Background fade", Range(0, 1)) = 0
+        _FrontOffset("Background offset", Range(0, 2)) = 0
+        _LineColor("Line color", Color) = (0, 0, 1, 1)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            fixed4 _BackColor, _FrontColor, _LineColor;
+            float _BackFade, _BackOffset, _FrontFade, _FrontOffset;
+            float _Thicc;
+            float offset;
+            float segmentCount;
+            
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex, _SegmentData;
+            float4 _MainTex_ST;
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed2 DrawLine(float4 seg, float2 coord)
+            {
+                coord.x -= offset;
+                float2 offset = float2(coord.x - seg.x, coord.y - seg.y);
+                float2 dir = float2(cos(seg.z), sin(seg.z));
+                float dist = dot(offset, dir * seg.w) / (seg.w);
+                dist = clamp(dist, 0, seg.w);
+                float2 projPoint = float2(seg.x, seg.y) + dist * dir;
+
+                float2 poffset = projPoint - coord;
+                float pointDist = dot(poffset, poffset);
+                
+                float output = step(pointDist, _Thicc * _Thicc);
+                
+                return float2(output, (1-output)*sign(poffset.y));
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                half index = tex2D(_MainTex, i.uv);
+                half4 segA = tex2D(_SegmentData, float2(index*segmentCount, 0));
+                half4 segB = tex2D(_SegmentData, float2((index + 1)*segmentCount, 0));
+                half4 segC = tex2D(_SegmentData, float2((index - 1)*segmentCount, 0));
+                half2 coord = i.uv;
+
+                float2 mainSeg = DrawLine(segA, coord);
+                float lineMult = max(mainSeg.x, DrawLine(segB, coord).x);
+                lineMult = max(lineMult, DrawLine(segC, coord).x);
+
+                fixed4 backColor = _BackColor * step(mainSeg.y, 0) * (1-i.uv.y + _BackOffset) * _BackFade;
+                fixed4 frontColor = _FrontColor * step(0, mainSeg.y) * (i.uv.y + _FrontOffset) * _FrontFade;
+
+                fixed4 col = lerp(max(backColor, frontColor), _LineColor, lineMult);
+                
+                return col;
+            }
+
+
+            ENDCG
+        }
+    }
+}
---
+++ Assets/GraphRenderer.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Mathematics;
+using Unity.Burst;
+
+using Random = UnityEngine.Random;
+
+public class GraphRenderer : MonoBehaviour
+{
+    public readonly static int SEGMENTS_ID = Shader.PropertyToID("segments");
+    public readonly static int OFFSET_ID = Shader.PropertyToID("offset");
+    public readonly static int SEGMENTCOUT_ID = Shader.PropertyToID("segmentCount");
+
+    [SerializeField]
+    private int maxSegments = 128;
+    [SerializeField]
+    private int resolution = 1024;
+    [SerializeField]
+    private Material mat;
+
+    private List<float2> graphPoints = new List<float2>();
+    private Texture2D segments;
+    private Texture2D lookup;
+
+    private bool hasChanged = false;
+    private float offset = 0;
+
+    public void Graph(float a, float b)
+    {
+        //store as points for extensibility
+        graphPoints.Add(new float2(a, b));
+        if (graphPoints.Count > maxSegments + 1)
+        {
+            graphPoints.RemoveRange(0, graphPoints.Count - maxSegments - 1);
+        }
+
+        hasChanged = true;
+    }
+
+    private void Awake()
+    {
+        lookup = new Texture2D(resolution, 1, TextureFormat.RFloat, false);
+        lookup.filterMode = FilterMode.Point;
+        mat.SetTexture("_MainTex", lookup);
+        segments = new Texture2D(maxSegments*4, 1, TextureFormat.RGBAFloat, false);
+        segments.filterMode = FilterMode.Point;
+        
+        mat.SetTexture("_SegmentData", segments);
+    }
+
+    private void Update()
+    {
+        if (hasChanged)
+        {
+            hasChanged = false;
+            UpdateGraph();
+        }
+
+        mat.SetFloat(OFFSET_ID, offset);
+        mat.SetFloat(SEGMENTCOUT_ID, 1f/(maxSegments * 4));
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Add random value"))
+        {
+            Graph(Random.value * 0.2f, Random.value);
+        }
+    }
+
+    private void UpdateGraph()
+    {
+        UpdateSegments();
+        UpdateSliceIDs();
+    }
+    
+    private void UpdateSegments()
+    {
+        var data = segments.GetRawTextureData<float4>();
+        float2 current = float2.zero;
+        //convert graph points to line segments for rendering
+        //segment data is stored as starting point in x and y, radians of segment direction in z and length in w
+        for (int i = 0; i < graphPoints.Count - 1; i++)
+        {
+            float2 start = current;
+            current.x += graphPoints[i].x;
+            current.y = graphPoints[i].y;
+            float2 direction = current - start;
+
+            float radians = math.asin(math.normalizesafe(direction).y);
+            
+            float length = math.sqrt(math.dot(direction, direction));
+
+            data[i] = new float4(start.x, start.y, radians, length);
+        }
+
+        offset = 1-current.x;
+        
+        segments.Apply();
+    }
+
+    private void UpdateSliceIDs()
+    {
+        var data = lookup.GetRawTextureData<float>();
+
+        LookupTextureJob job = new LookupTextureJob() { data = data, offset = offset, resolution = resolution, segments = segments.GetRawTextureData<float4>() };
+
+        job.Schedule(data.Length, 128).Complete();
+
+        lookup.Apply();
+    }
+}
+
+[BurstCompile]
+public struct LookupTextureJob : IJobParallelFor
+{
+    [WriteOnly]
+    public NativeArray<float> data;
+    public float resolution, offset;
+    [ReadOnly]
+    public NativeArray<float4> segments;
+
+    public void Execute(int index)
+    {
+        float currentOffset = (float)index / resolution - offset;
+
+        //map segment indices onto the lookup texture
+        for (int j = 0; j < segments.Length; j++)
+        {
+            float segmentStart = segments[j].x;
+            float segmentEnd = segments[j].x + math.cos(segments[j].z) * segments[j].w;
+
+            if (segmentStart < currentOffset && segmentEnd > currentOffset)
+            {
+                data[index] = j;
+                break;
+            }
+        }
+    }
+}
